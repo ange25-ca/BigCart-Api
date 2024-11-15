24b62e3c2eac506ded767f4437605fd83d8eb8e7
Ange <monsecahuum@gmail.com>
2024-11-15 00:02:34 -0600

    Se ajusta la obtención del usuario por lo que se agrega el id del mismo

--- src/services/usuarioServices.ts
+++ src/services/usuarioServices.ts
@@ -1,23 +1,5 @@
 import { SignUp, obtenerPorCorreo, obtenerTodosLosUsuarios } from '../models/usuarioModel';
-{/*
-export async function registrarUsuario(username: string, lastname: string, age: number, email: string, 
-    phonenumber: number, adress: string, password: string): Promise<void> {
-    try {
-        await SignUp(username, lastname, age, email, phonenumber, adress, password);
-    } catch (error) {
-        console.error('Error al registrar usuario en el servicio:', error);
-        throw error;
-    }
-}
 
-export async function obtenerPorcorreo(email: string): Promise<any> {  
-    try {
-        return await obtenerPorCorreo(email);
-    } catch (error) {
-        console.error('Error al obtener usuario por nombre en el servicio:', error);
-        throw error;
-    }
-}*/}
 
 export async function obtenerUsuarios(): Promise<any[]> {  
     try {
