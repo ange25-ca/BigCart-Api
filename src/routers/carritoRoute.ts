f32c092cd4cb6831def0a9b806e5d9c9d293e69d
antoniouc <antoniouc38@gmail.com>
2024-12-12 20:49:11 -0600

    Se agrega funcionalidad para poder vaciar el carrito .

--- src/routers/carritoRoute.ts
+++ src/routers/carritoRoute.ts
@@ -1,9 +1,10 @@
 import { Router } from "express";
-import { addToCart, eliminarXProducto, updateCartQuantity, viewAllCartProductos } from "../controller/carritoController";
+import { addToCart, eliminarXProducto, updateCartQuantity, viewAllCartProductos,vaciarCart } from "../controller/carritoController";
 const router: Router = Router();
 router.post('/addCart', addToCart);
 router.get('/viewCart/:idCart',viewAllCartProductos );
 router.put('/updatequantity', updateCartQuantity)
 router.delete('/eliminarProducto', eliminarXProducto);
+router.put('/vaciarCarrito', vaciarCart)
 export default router;
 
